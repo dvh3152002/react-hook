8595e4a023296f6543a83256c08e237db689d090
dvh3152002 <dvh31502@gmail.com>
2023-03-06 23:27:16 +0700

    Render Dữ Liệu của Biến với React Hook | React Season 2 Với Hook Cho Beginner Từ A đến Z

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name='abc';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello 123</h1>
+        <h1>Hello {name}</h1>
       </header>
     </div>
   );
