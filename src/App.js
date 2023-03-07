d486901acde3be4cfc5713283104dea553b60bdd
dvh3152002 <dvh31502@gmail.com>
2023-03-07 22:57:44 +0700

    Multiple Components với React Hook | React Season 2 Với Hook Cho Beginner Từ A đến Z

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
 
 function App() {
   let name='abc';
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello {name}</h1>
---
+++ src/views/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css';
+
+const Nav=()=>{
+    return(
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#">Dropdown</a></li>
+        </ul>
+    );
+}
+
+export default Nav;
