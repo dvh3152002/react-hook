701b96b7183d7c37d5797f5080720a32728e25e7
dvh3152002 <dvh31502@gmail.com>
2023-03-09 23:02:18 +0700

    10.Outputting Lists - Renders Loop Với React Hook | React Season 2 Với Hook Cho Beginner Từ A đến Z

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
-function App() {
-  let name='abc';
+const App=()=> {
+  let [name,setName]=useState('adasd');
+  let [address,setAddress]=useState('');
+  let [todos,setTodos]=useState([]);
+
+  const handleOnClick=()=>{
+    if(!address){
+      alert("empty input");
+    }else{
+      let todo={id:'1',title:address};
+      setAddress("");
+      setTodos([...todos,todo]);
+    }
+  }  
+
+  const handleOnChange=(event)=>{
+    setAddress(event.target.value);
+    console.log(address)
+  }
+
   return (
     <div className="App">
       <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello {name}</h1>
+        {todos && todos.length>0 &&
+          todos.map((item,index)=>{
+            return(
+              <li key={index}>{item.title}</li>
+            );
+          })
+        }
+        <input type='text' value={address} onChange={(event)=>handleOnChange(event)}/>
+        <button onClick={handleOnClick}>click</button>
       </header>
     </div>
   );
