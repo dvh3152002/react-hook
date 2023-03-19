7d48e893cfaa497b00e8024a993eb4a05ae0f25d
dvh3152002 <dvh31502@gmail.com>
2023-03-19 22:54:17 +0700

    Reusing Custom Hooks - Tạo Trang Xem Chi Tiết Blogs | React Season 2 Với Hook Cho Beginner

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,17 +1,25 @@
 import { useParams,useNavigate } from "react-router-dom";
+import useFetch from "../customize/fetch";
 
 const DetailBlog=()=>{
     let {id}=useParams();
     let navigate = useNavigate();
+    const {data:dataBlogDetail,isLoading,isError}=useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`,false);
 
     const handleBackDetailBlog=()=>{
         navigate("/blog");
     }
-
     return(
         <>
             <div><span onClick={()=>handleBackDetailBlog()}>&lt;-- Back</span></div>
-            <div>detail {id}</div>
+            <div className="data-blog">
+                {dataBlogDetail && 
+                    <>
+                        <div className="title">Id:{dataBlogDetail.id} --- {isLoading===true ? 'Loading data':dataBlogDetail.title}</div>
+                        <div className="content">{dataBlogDetail.body}</div>
+                    </>
+                }
+            </div>
         </>
     )
 }
