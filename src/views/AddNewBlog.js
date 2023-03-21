5892851da698d8d25b45f0c6baa83e4bdb5fac75
dvh3152002 <dvh31502@gmail.com>
2023-03-21 22:11:43 +0700

    Making a POST Request/ Deleting data - Tạo/Xóa Blogs React| React Season 2 Với Hook Cho Beginner

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -1,19 +1,31 @@
 import './Blog.scss';
 import { useState } from 'react';
+import axios from 'axios';
 
-const AddNewBlog=()=>{
+const AddNewBlog=(props)=>{
     const [title,setTitle]=useState('');
     const [content,setContent]=useState('');
 
-    const addNewBlog=(event)=>{
+    const addNewBlog=async(event)=>{
         if(!title)  alert('empty title');
         if(!content)  alert('empty content');
+        let data={
+            title:title,
+            body:content,
+            userId:1
+        }
+
+        let res=await axios.post('https://jsonplaceholder.typicode.com/posts',data);
+        if(res && res.data){
+            let newBlog=res.data;
+            props.handleAddNew(newBlog);
+        }
     }
 
     return(
         <div className="add-new-container">
             <h3 className='add-new-text'>--- Add new Blog ---</h3>
-            <form onSubmit={addNewBlog}>
+            {/* <form onSubmit={addNewBlog}> */}
                 <div className='input-data'>
                     <label>Title:</label>
                     <input type='text' value={title} onChange={(event)=>setTitle(event.target.value)}></input>
@@ -22,9 +34,9 @@ const AddNewBlog=()=>{
                     <label>Content:</label>
                     <input type='text' value={content} onChange={(event)=>setContent(event.target.value)}></input>
                 </div>
-                {/* <button className='btn-add-new' onClick={addNewBlog}>Submit</button> */}
-                <button className='btn-add-new' type='submit'>Submit</button>
-            </form>
+                <button className='btn-add-new' onClick={addNewBlog}>Submit</button>
+                {/* <button className='btn-add-new' type='submit'>Submit</button> */}
+            {/* </form> */}
         </div>
     );
 }
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,32 +1,64 @@
 import useFetch from "../customize/fetch";
 import './Blog.scss';
-import { Link,useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from "react";
+import AddNewBlog from "./AddNewBlog";
 
 const Blog=()=>{
-    let newData=[];
-    let navigate=useNavigate();
+    const [show, setShow] = useState(false);
+    const [newData,setNewData]=useState([]);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const {data:dataBlogs,isLoading,isError}=useFetch(`https://jsonplaceholder.typicode.com/posts`,false);
 
-    if(dataBlogs && dataBlogs.length>0){
-        newData=dataBlogs.slice(0,9);
+    useEffect(()=>{
+        if(dataBlogs && dataBlogs.length>0){
+            let data=dataBlogs.slice(0,9);
+            setNewData(data)
+        }
+    },[dataBlogs]);
+
+    const handleAddNew=(blog)=>{
+        let dataCopy=newData;
+        dataCopy.unshift(blog);
+        setShow(false);
+        setNewData(dataCopy);
     }
 
-    const handleAddNew=()=>{
-        navigate('/add-new-blog');
+    const deleteBlog=(id)=>{
+        let data=newData;
+        data=data.filter(item=>item.id!==id);
+        setNewData(data);
     }
 
     return(
         <>
-            <div>
-                <button className="btn-add-new" onClick={handleAddNew}>+Add new blog</button>
-            </div>
+            <Button variant="primary" className="my-3" onClick={handleShow}>
+                +Add new blog
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Add New Blog</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddNewBlog 
+                        handleAddNew={handleAddNew}
+                    />
+                </Modal.Body>
+            </Modal>
             <div className="blog-container">
                 {isLoading===false && newData && newData.length>0 &&
                     newData.map((item,index)=>{
                         return(
                             <div key={index} className="single-blog">
-                                <div className="title">{item.title}</div>
+                                <div className="title">
+                                    <span>{item.title} </span>
+                                    <span onClick={()=>deleteBlog(item.id)}>x</span>
+                                </div>
                                 <div className="content">{item.body}</div>
                                 <Link to={`/blog/${item.id}`}>View detail</Link>
                             </div>
